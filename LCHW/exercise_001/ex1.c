e11275a35022a82b315d6d0913e76eb618b19025
ykDu <ykdu91@gmail.com>
2019-03-21 23:36:03 +0800

    upload ex1.c

---
+++ LCHW/exercise_001/ex1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main(int argc, char* argv[])
+{
+    int distance = 100;
+
+    // this is also comment
+    printf("You are %d miles away.\n", distance);
+    printf("You are %d miles away.\n", distance * 2);
+    printf("You are %d miles away.\n", distance * 3);
+    printf("You are %d miles away.\n", distance * 4);
+    printf("You are %d miles away.\n", distance * 5);
+
+    return 0;
+}
