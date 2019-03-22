7ec2064e39a5e581ab0bd3b32b6ead26989ab533
ykDu <ykdu91@gmail.com>
2019-03-23 01:02:00 +0800

    update ex1

--- LCHW/exercise_001/ex1.c
+++ LCHW/exercise_001/ex1.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
 
-/* This is a comment. */
 int main(int argc, char* argv[])
 {
-    int distance = 100;
-
-    // this is also comment
-    printf("You are %d miles away.\n", distance);
-    printf("show addr. of distance in %%d %d\n", &distance);
-    printf("show addr. of distance in %%p %p\n", &distance);
-    printf("show addr. of fucntion main in %%p %p\n", main);
+    puts("Hello World!\n");
+    printf("return value of puts: %d\n", puts("Hello\n\tWold!\n"));
+    char str0[] = {0x41, 0x31, 0x0};
+    putchar(str0[0]); 
+    printf("return value of putchar: %d\n", putchar(258u));
+    printf("return value of putchar: %f\n", 258.f);
+    // UB    
+    /*  
+        LC6:
+            .ascii "return value of putchar: %c %f %d %p %s %d %s %s \12\0"
+        .loc 1 12 5
+        movl	$97, 4(%esp)
+        movl	$LC6, (%esp)
+        call	_printf
+     */
+    printf("return value of putchar: %c %f %d %p %d %s\n\0", 0x61);
+    putchar(*(str0+1)); 
+    putchar(*(str0+2)); 
+    putchar('|'); 
+    putchar('\n'); 
+    int a = 'aa';
+    printf("NXPH ~ %d", a);
+    /*int b;*/
+    /*scanf("read from user %d", &b);*/
+    /*printf("read from user: %d", b);*/
     return 0;
 }
+/*
+ * 1. 删除 ex1.exe 中的相关文字
+ *
+ *
+ */
