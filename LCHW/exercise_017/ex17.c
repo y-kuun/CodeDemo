38c709834c4485eb7d2eedf974720fd29008003e
ykdu <ykdu91@gmail.com>
2019-03-28 21:24:40 +0800

    modify ex17

--- LCHW/exercise_017/ex17.c
+++ LCHW/exercise_017/ex17.c
@@ -3,168 +3,102 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "database.h"
 
-#define MAX_DATA 512
-#define MAX_ROWS 100
+void parser_cmd_line(char cmdv[][128], int *para_cnt){
+    int max_data = 128;
+    int max_argc = 10;
 
-typedef struct {
-    int id;
-    int set;
-    char name[MAX_DATA];
-    char email[MAX_DATA];
-} Address;
-
-typedef struct {
-    Address rows[MAX_ROWS];
-} Database;
-
-typedef struct {
-    FILE *file;
-    Database *db;
-} Connection;
-
-void die(const char *message){
-    if(errno){
-        perror(message); 
-    } else {
-        printf("ERROR: %s\n", message);
-    }
-    exit(1);
-}
-
-void Address_print(Address *addr){
-    printf("%d %s %s\n", addr->id, addr->name, addr->email);
-}
-
-void Database_load(Connection *conn){
-    int rc = fread(conn->db, sizeof(Database), 1, conn->file);
-    if(rc != 1){
-        die("Failed to load database.");
-    }
-}
-
-Connection* Database_open(const char *filename, char mode){
-    Connection *conn = malloc(sizeof(Connection));
-    if(!conn) die("Memory error");
-
-    conn->db = malloc(sizeof(Database));
-    if(!conn->db) die("Memory error");
-
-    if(mode == 'c'){
-        conn->file = fopen(filename, "w"); 
-    } else {
-        conn->file = fopen(filename, "r+"); 
-        if(conn->file){
-            Database_load(conn); 
+    int data_cnt = 0;
+    *para_cnt = 0;
+    char ch;
+    while((ch = getchar())){
+        if(strchr(" \n\t", ch)){
+            if(data_cnt > 0){
+                cmdv[*para_cnt][data_cnt] = '\0';
+                /*printf("\nfinish read arg %d == %s\n", *para_cnt, cmdv[*para_cnt]);*/
+                data_cnt = 0;
+                *para_cnt += 1;
+                assert(*para_cnt < max_argc);
+            }        
+            if(ch == '\n'){
+                break;
+            }
+            continue;
         }
-    }
-    if(!conn->file) die("Failed to open the file");
-
-    return conn;
-}
-
-void Database_close(Connection *conn){
-    if(conn){
-        if(conn->file) fclose(conn->file);  
-        if(conn->db) free(conn->db);
-        free(conn);
-    }    
-}
-
-void Database_write(Connection *conn){
-    rewind(conn->file);
-    
-    int rc = fwrite(conn->db, sizeof(Database), 1, conn->file);
-    if(rc != 1) die("Failed to write datebase.");
-    
-    rc = fflush(conn->file);
-    if(rc != 0) die("Cannot flush datebase.");
-}
-
-void Database_create(Connection *conn){
-    int i = 0;
-    for(i = 0; i < MAX_ROWS; i++){
-        Address addr = {.id = i, .set = 0};
-        conn->db->rows[i] = addr;
+        /*printf("%c ", ch);*/
+        cmdv[*para_cnt][data_cnt] = ch;
+        data_cnt++;
+        assert(data_cnt < max_data -1);
     }
 }
 
-void Database_set(Connection *conn, int id, const char *name, const char *email){
-    Address *addr = &conn->db->rows[id];
-    if(addr->set) die("Already set, delete it first");
-    
-    addr->set = 1;
-    char *res = strncpy(addr->name, name, MAX_DATA);
-    if(!res) die("Name copy failed");
-
-    res = strncpy(addr->email, email, MAX_DATA);
-    if(!res) die("Email copy failed");
-}
-
-void Database_get(Connection *conn, int id){
-    Address *addr = &conn->db->rows[id];
-    if(addr->set){
-        Address_print(addr);
-    } else {
-        die("ID is not set"); 
-    }
-}
-
-void Database_delete(Connection *conn, int id){
-    Address addr = {.id = id, .set = 0};
-    conn->db->rows[id] = addr;
-}
-
-void Database_list(Connection *conn){
-    int i = 0;
-    Database *db = conn->db;
-    for(i = 0; i < MAX_ROWS; i++){
-        Address *cur = &db->rows[i];
-        if(cur->set){
-            Address_print(cur); 
-        }
-    }
-}
 
 int main(int argc, char* argv[])
 {
     if(argc < 3) {
-        die("USAGE: ex17 <dbfile> <action> [action params]"); 
+        die("USAGE: ex17 <dbfile> <MAX_DATA> <MAX_ROWS>", NULL); 
     }
     char *filename = argv[1];
-    char action = argv[2][0];
-    Connection *conn = Database_open(filename, action);
+    int max_data = atoi(argv[2]);
+    int max_rows = atoi(argv[3]);
+    Connection *conn = NULL;
     int id = 0;
+    int loop = 1; 
+    // cmd buff
+    char cmdv[10][128];
+    memset(cmdv, 0, 10 * 128 * sizeof(char));
 
-    if(argc > 3) id = atoi(argv[3]);
-    if(id >= MAX_ROWS) die("There's not that many records.");
-
-    switch(action) {
-        case 'c': 
-            Database_create(conn);
-            Database_write(conn);
-            break; 
-        case 'g': 
-            if(argc != 4) die("Need an id to get");
-            Database_get(conn, id);
-            break; 
-        case 's': 
-            if(argc != 6)  die("Need id, name, email to set");
-            Database_set(conn, id, argv[4], argv[5]);
-            Database_write(conn);
-            break; 
-        case 'd': 
-            if(argc != 4) die("Need an id to delete");
-            Database_delete(conn, id);
-            Database_write(conn);
-            break; 
-        case 'l': 
-            Database_list(conn);
-            break; 
-        default:
-            die("Invalid action, only: c=create, g=get, s=set, d=del, l=list");
+    printf("Welcome to Simple Database !!! \n");
+    while(loop){
+        printf(">> ");
+        int para_cnt = 0;
+        char action = getchar();
+        parser_cmd_line(cmdv, &para_cnt);
+        if(conn == NULL){
+            // if connection not initiated, then do it
+            conn = Database_open(filename, action, max_data, max_rows); 
+        }
+        if(strchr("gsd", action)){
+            id = atoi(cmdv[0]);
+            if(id <  0 || id >= max_rows){
+                printf("id: %d is not in [0, %d)\n", id, max_rows);
+                continue;
+            }
+        }
+        switch(action) {
+            case 'c':
+                Database_create(conn);
+                Database_write(conn);
+                break; 
+            case 'g': 
+                if(para_cnt != 1) die("Need an id to get", conn);
+                Database_get(conn, id);
+                break; 
+            case 'f': 
+                if(para_cnt != 2)  die("Need name, email to find", conn);
+                Database_find(conn, cmdv[1], cmdv[2]);
+                break; 
+            case 's': 
+                if(para_cnt != 3)  die("Need id, name, email to set", conn);
+                Database_set(conn, id, cmdv[1], cmdv[2]);
+                Database_write(conn);
+                break; 
+            case 'd': 
+                if(para_cnt != 1) die("Need an id to delete", conn);
+                Database_delete(conn, id);
+                Database_write(conn);
+                break; 
+            case 'l': 
+                Database_list(conn);
+                break; 
+            case 'x':
+                loop = 0;
+                break;
+            default:
+                printf("Invalid action, only: c=create, g=get, f=find, s=set, d=del, l=list\n");
+        }
     }
     Database_close(conn);
-
     return 0;
 }
