951a6f616b527c5a3eb49d01740ecb521d450270
ykdu <ykdu91@gmail.com>
2019-05-12 23:47:24 +0800

    update for hashmap

---
+++ LCHW/exercise_037/src/lcthw/hashmap.h
@@ -0,0 +1,36 @@
+#ifndef __HASHMAP_H__
+#define __HASHMAP_H__
+
+#include <stdint.h>
+#include <lcthw/darray.h>
+
+#define DEFAULT_NUMBER_OF_BUCKETS 100
+
+typedef int (*Hashmap_compare)(void*, void*);
+typedef uint32_t (*Hashmap_hash)(void *key);
+
+typedef struct Hashmap {
+    DArray *buckets;
+    Hashmap_compare compare;
+    Hashmap_hash hash;
+} Hashmap;
+
+typedef struct HashmapNode {
+    void *key;
+    void *data;
+    uint32_t hash;
+} HashmapNode;
+
+typedef int (*Hashmap_traverse_cb)(HashmapNode *node);
+
+Hashmap *Hashmap_create(Hashmap_compare compare, Hashmap_hash);
+void Hashmap_destroy(Hashmap *map);
+
+int Hashmap_set(Hashmap *map, void *key, void *data);
+void *Hashmap_get(Hashmap *map, void *key);
+
+int Hashmap_treaverse(Hashmap *map, Hashmap_traverse_cb traverse_cb);
+
+void *Hashmap_delete(Hashmap *map, void *key);
+
+#endif
