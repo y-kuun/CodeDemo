5aacdd2eb9b5cb1a9904af20d13bccb3fca5805c
ykdu <ykdu91@gmail.com>
2019-06-09 21:36:51 +0800

    update for ex47

---
+++ LCHW/exercise_047/src/lcthw/stats.h
@@ -0,0 +1,25 @@
+#ifndef __LCTHW_STATS_H__
+#define __LCTHW_STATS_H__
+
+typedef struct Stats
+{
+    double sum;
+    double sumsq;
+    unsigned long n;
+    double min;
+    double max;
+} Stats;
+
+Stats *Stats_recreate(double sum, double sumsq, unsigned long n, double min, double max);
+
+Stats *Stats_create();
+
+double Stats_mean(Stats *st);
+
+double Stats_stddev(Stats *st);
+
+void Stats_sample(Stats *st, double s);
+
+void Stats_dump(Stats *st);
+
+#endif
---
+++ LCHW/exercise_047/tests/lib_main.c
@@ -0,0 +1,5 @@
+#include <lcthw/dbg.h>
+
+int main(int argc, char *argv[]){
+    return 0;
+}
